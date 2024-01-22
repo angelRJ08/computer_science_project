bf4229e568385cd6e338e6f7914d617a63cb98dc
angelRJ08 <154650658+angelRJ08@users.noreply.github.com>
2024-01-22 14:37:10 -0600

    pratikjoin

--- main.ts
+++ main.ts
@@ -1,1 +1,106 @@
-
+function spawnenemyplayer () {
+    spawnenemieslocation = tiles.getTilesByType(sprites.dungeon.collectibleInsignia)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+    for (let index = 0; index < 10; index++) {
+        enemysprite = sprites.create(list._pickRandom(), SpriteKind.Enemy)
+        tiles.placeOnTile(enemysprite, spawnenemieslocation.removeAt(randint(0, spawnenemieslocation.length - 1)))
+    }
+}
+let spawnenemieslocation: tiles.Location[] = []
+let list: Sprite[] = []
+let enemysprite: Sprite = null
+let random_list_of_arrays = enemysprite
+info.setScore(0)
+tiles.setCurrentTilemap(tilemap`level2`)
+list = [sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    `, SpriteKind.Enemy), sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . 5 5 . . . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . . 5 5 5 5 5 5 5 5 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy), sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . 7 . . . . . . . . . . . . 
+    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+    . . 7 . . 7 7 7 7 7 7 7 7 7 . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)]
+let mySprite = sprites.create(img`
+    . . . f f f f f f . . . . . . . 
+    . . . f f 2 2 c c f f . . . . . 
+    . . . . f f f c c c c f f f . . 
+    . . . . c c 2 4 4 4 2 2 2 2 c c 
+    . . . c 9 9 b 2 2 2 2 2 2 2 2 2 
+    . . c 9 9 9 1 1 1 b 2 2 2 2 2 2 
+    . c 2 b b 1 1 1 9 9 2 2 2 c c c 
+    c 2 2 2 2 2 2 2 2 2 2 2 c 2 2 2 
+    f 2 2 2 2 2 2 2 2 2 2 2 4 4 2 2 
+    . f 2 2 2 2 2 2 2 2 2 2 2 4 4 4 
+    . . f f 2 2 2 2 2 2 2 c f f c 4 
+    . . . . f f f f f f f c f f f f 
+    . . . . . . . . . f 2 c 2 f f f 
+    . . . . . . . . . f c c 2 f f f 
+    . . . . . . . . . . f f f f f f 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+let mySprite2 = sprites.create(img`
+    . . . . . . 8 8 c c 8 8 . . . . 
+    . . . . . 8 6 6 6 6 6 6 8 . . . 
+    . . . . 6 c 6 6 6 6 6 6 c 6 . . 
+    . . . 8 6 c 9 6 6 6 6 6 c 6 8 . 
+    . . . f 6 6 9 6 6 6 6 6 c 6 f . 
+    . . . f 6 6 9 6 6 6 6 6 6 6 f . 
+    . . . f 6 6 9 6 6 6 6 6 6 6 f . 
+    . . . f 6 c 6 9 9 6 6 6 c 6 f . 
+    . . . 8 6 c 8 c c c c 8 c 6 8 . 
+    . . . 8 6 8 c b b b b c 8 6 8 . 
+    . . . 8 6 8 b b b b b b 8 6 8 . 
+    . . . 8 8 8 8 8 8 8 8 8 8 8 8 . 
+    . . . f 8 d 8 8 8 8 8 8 d 8 f . 
+    . . . f 8 6 d 8 8 8 8 d 6 8 f . 
+    . . . f f 8 8 8 8 8 8 8 8 f f . 
+    . . . . f f . . . . . . f f . . 
+    `, SpriteKind.Player)
+controller.player1.moveSprite(mySprite)
+controller.player2.moveSprite(mySprite2)
+scene.cameraFollowSprite(mySprite)
+spawnenemyplayer()

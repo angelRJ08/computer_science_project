e47f3d162dc4cdadebdb1916d9c7eee72a5f61f7
Pratik Paduri <87135097+hilobot4106@users.noreply.github.com>
2024-01-23 16:07:14 -0600

    made sprites spawn

--- main.ts
+++ main.ts
@@ -1,4 +1,36 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+namespace SpriteKind {
+    export const enemy1 = SpriteKind.create()
+}
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 2 2 . . . . . . . 2 2 . . . 
+        . . . 2 2 . . . . . 2 . 2 . . . 
+        . . . . 2 2 . . . 2 . . 2 . . . 
+        . . . . 2 2 2 . . 2 2 2 2 . . . 
+        . . . . 2 2 . 2 2 2 2 . . . . . 
+        . . . . . 2 2 . 2 . 2 . . . . . 
+        . . . . . . . . 2 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, player2, 0, -60)
+})
+controller.player2.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    player2.setVelocity(100, 0)
+})
+controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    player2.setVelocity(-100, 0)
+})
+controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    player1.setVelocity(100, 0)
+})
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -18,78 +50,32 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         `, player1, 0, -60)
 })
-controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
-    player1.setVelocity(100, 0)
-})
+function spawnenemy (num: number) {
+    for (let index = 0; index < num; index++) {
+        enemysprite = sprites.create(enemylist._pickRandom(), SpriteKind.Enemy)
+        tiles.placeOnTile(enemysprite, spawnenemieslocation._pickRandom())
+        pause(500)
+    }
+}
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    sprites.destroy(player1)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
 })
 controller.player1.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
     player1.setVelocity(-100, 0)
 })
+let enemysprite: Sprite = null
 let projectile: Sprite = null
+let player2: Sprite = null
+let enemylist: Image[] = []
+let spawnenemieslocation: tiles.Location[] = []
 let player1: Sprite = null
-let enemysprite = 0
 let mySprite: Sprite = null
-let random_list_of_arrays = enemysprite
 info.setLife(3)
 tiles.setCurrentTilemap(tilemap`level2`)
-player1 = sprites.create(img`
-    . . . f f f f f f . . . . . . . 
-    . . . f f 2 2 c c f f . . . . . 
-    . . . . f f f c c c c f f f . . 
-    . . . . c c 2 4 4 4 2 2 2 2 c c 
-    . . . c 9 9 b 2 2 2 2 2 2 2 2 2 
-    . . c 9 9 9 1 1 1 b 2 2 2 2 2 2 
-    . c 2 b b 1 1 1 9 9 2 2 2 c c c 
-    c 2 2 2 2 2 2 2 2 2 2 2 c 2 2 2 
-    f 2 2 2 2 2 2 2 2 2 2 2 4 4 2 2 
-    . f 2 2 2 2 2 2 2 2 2 2 2 4 4 4 
-    . . f f 2 2 2 2 2 2 2 c f f c 4 
-    . . . . f f f f f f f c f f f f 
-    . . . . . . . . . f 2 c 2 f f f 
-    . . . . . . . . . f c c 2 f f f 
-    . . . . . . . . . . f f f f f f 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
 scene.cameraFollowSprite(player1)
+spawnenemieslocation = tiles.getTilesByType(assets.tile`myTile0`)
 info.setLife(3)
-tiles.setCurrentTilemap(tilemap`level2`)
-let list = [sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . 2 2 2 2 2 . 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    `, SpriteKind.Enemy), sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . 5 5 . . . . 
-    . . . 5 5 5 5 5 5 5 5 5 5 . . . 
-    . . . 5 5 5 5 5 5 5 5 5 5 . . . 
-    . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-    . . . . 5 5 5 5 5 5 5 5 . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Enemy), sprites.create(img`
+enemylist = [img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . 7 . . . . . . . . . . . . 
@@ -106,7 +92,41 @@ let list = [sprites.create(img`
     . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
     . . 7 . . 7 7 7 7 7 7 7 7 7 . . 
     . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Enemy)]
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . 5 5 . . . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . . 5 5 5 5 5 5 5 5 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . 3 3 3 . 
+    . . . . . 3 3 3 3 3 3 3 3 . . . 
+    3 3 3 3 3 3 3 . . . . . . . . . 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 . . . 
+    . . . . 3 . . . . 3 3 . . 3 . . 
+    . . . . 3 3 3 3 3 . . . . . . . 
+    . . 3 3 3 3 3 3 3 3 3 3 . . . . 
+    . . . 3 . . . . . . . . 3 3 3 . 
+    . . . 3 . . . . . . . . . . . 3 
+    . . . 3 . . . . . . . . 3 3 3 . 
+    . . . 3 . . 3 3 3 3 3 3 . . . . 
+    . . 3 3 3 3 . . . . . . . . . . 
+    `]
 player1 = sprites.create(img`
     . . . f f f f f f . . . . . . . 
     . . . f f 2 2 c c f f . . . . . 
@@ -125,7 +145,7 @@ player1 = sprites.create(img`
     . . . . . . . . . . f f f f f f 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-let player2 = sprites.create(img`
+player2 = sprites.create(img`
     . . . . . . 8 8 c c 8 8 . . . . 
     . . . . . 8 6 6 6 6 6 6 8 . . . 
     . . . . 6 c 6 6 6 6 6 6 c 6 . . 
@@ -144,5 +164,7 @@ let player2 = sprites.create(img`
     . . . . f f . . . . . . f f . . 
     `, SpriteKind.Player)
 scene.cameraFollowSprite(player1)
-player1.setPosition(14, 97)
-player2.setPosition(131, 92)
+player1.setPosition(1, 112)
+player2.setPosition(159, 112)
+sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+spawnenemy(10)
